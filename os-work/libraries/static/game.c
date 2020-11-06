e7f514ab0dcc290972a53ba999c04ae180a2d0c5
Moses <loschnigsolutions@gmail.com>
2020-11-06 18:16:56 +0530

    OS: Creation of Static Library

---
+++ os-work/libraries/static/game.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include "test.h"
+
+void main()
+{
+	print_hello();
+
+	fact(5);
+
+	add(5,10);
+
+	print_goodbye();
+}
---
+++ os-work/libraries/static/lib/test.h
@@ -0,0 +1,14 @@
+
+void print_hello();
+
+void print_goodbye();
+
+void power(int,int);
+
+void fact(int);
+
+void add(int, int);
+
+void sub(int,int);
+
+void mul(int,int);
